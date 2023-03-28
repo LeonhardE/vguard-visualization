045249bdcc03dc2c48df316235ec12d14297e146
LeonhardE <31626796+LeonhardE@users.noreply.github.com>
2023-03-27 21:52:17 -0400

    naive ordering phase

--- src/Component/Util.js
+++ src/Component/Util.js
@@ -1,7 +1,28 @@
 import { createTheme } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
 
 export const lightTheme = createTheme({
     palette: {
       mode: 'light',
     },
   });
+
+export const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
+export const Message = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
+
+  
--- src/Component/VGuard.js
+++ src/Component/VGuard.js
@@ -1,19 +1,17 @@
 import React from 'react';
-// import { useState } from 'react'
+import { useState } from 'react'
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { ThemeProvider } from '@mui/material/styles';
-import { lightTheme } from './Util';
+import { Item, lightTheme, Message } from './Util';
 import CssBaseline from '@mui/material/CssBaseline';
-// import Fab from '@mui/material/Fab';
-// import AddIcon from '@mui/icons-material/Add';
-// import RemoveIcon from '@mui/icons-material/Remove';
+import Grid from '@mui/material/Grid';
 
 export default function VGuard() {
-    // const [validator, setValidator] = useState(4);
+    const [level, setlevel] = useState(0);
 
     return (
         // <Box sx={{ '& > :not(style)': { m: 1 } }}>
@@ -54,7 +52,7 @@ export default function VGuard() {
                     VGuard Visualization
                     </Typography>
                     <Typography variant="h5" align="center" color="text.secondary" paragraph>
-                    This is visualization system for VGuard. You can observe the message passing and consensus progress through
+                    This is visualization system for VGuard. You can observe the ordering and consensus progress through
                     this webpage. More features are coming soon!
                     </Typography>
                     <Stack
@@ -63,23 +61,167 @@ export default function VGuard() {
                     spacing={2}
                     justifyContent="center"
                     >
-                        <Button variant="contained" >Next Step</Button>
-                        <Button variant="outlined" >Restart</Button>
+                        <Button variant="contained" onClick={() => {
+                            setlevel(level + 1)
+                        }} >Next Step</Button>
+                        <Button variant="outlined" onClick={() => {
+                            setlevel(0)
+                        }} >Restart</Button>
                     </Stack>
                 </Container>
             </Box>
-            <Container maxWidth="sm">
-                <Stack
+            <Container maxWidth="lg">
+                {/* <Stack
                 sx={{ pt: 4 }}
                 direction="column"
                 spacing={8}
                 justifyContent="center"
                 >
-                    <img class="vehicle" alt="vehicle" src="car.png" />
-                    <img class="vehicle" alt="vehicle" src="car.png" />
-                    <img class="vehicle" alt="vehicle" src="car.png" />
-                    <img class="vehicle" alt="vehicle" src="car.png" />
-                </Stack>
+                    <img className="vehicle" alt="vehicle" src="car.png" />
+                    <img className="vehicle" alt="vehicle" src="car.png" />
+                    <img className="vehicle" alt="vehicle" src="car.png" />
+                    <img className="vehicle" alt="vehicle" src="car.png" />
+                </Stack> */}
+                <Grid 
+                    container 
+                    justifyContent="center" 
+                    alignItems="center" 
+                    spacing={2}>
+                    <Grid item xs={3}>
+                        <img className="vehicle" alt="vehicle" src="car.png" />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <img className="vehicle" alt="vehicle" src="car.png" />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <img className="vehicle" alt="vehicle" src="car.png" />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <img className="vehicle" alt="vehicle" src="car.png" />
+                    </Grid>
+                    {/* vehicle identities */}
+                    <Grid item xs={3}>
+                        <Item>Proposer</Item>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Item>Validator</Item>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Item>Validator</Item>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Item>Validator</Item>
+                    </Grid>
+                    {/* Message level 1 */}
+                    <Grid item xs={3} display={level > 0 ? "block" : "none"}>
+                        <Message>Send PreOrder Messages to all Validators <br />
+                            (Pre-Order, Bi, hBi, Vo, hVo, i, σVp)</Message>
+                    </Grid>
+                    <Grid item xs={3} display={level > 0 ? "block" : "none"}>
+                    </Grid>
+                    <Grid item xs={3} display={level > 0 ? "block" : "none"}>
+                    </Grid>
+                    <Grid item xs={3} display={level > 0 ? "block" : "none"}>
+                    </Grid>
+                    {/* Message level 2 */}
+                    <Grid item xs={3} display={level > 1 ? "block" : "none"}>
+                    </Grid>
+                    <Grid item xs={3} display={level > 1 ? "block" : "none"}>
+                        <Message>Receive PreOrder Message <br />
+                            Start to verify
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} display={level > 1 ? "block" : "none"}>
+                        <Message>Receive PreOrder Message <br />
+                            Start to verify
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} display={level > 1 ? "block" : "none"}>
+                        <Message>Receive PreOrder Message <br />
+                            Start to verify
+                        </Message>
+                    </Grid>
+                    {/* Message level 3 */}
+                    <Grid item xs={3} display={level > 2 ? "block" : "none"}>
+                    </Grid>
+                    <Grid item xs={3} display={level > 2 ? "block" : "none"}>
+                        <Message>If verified, reply to the proposer <br />
+                            (PO-Reply, i, σVi)
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} display={level > 2 ? "block" : "none"}>
+                        <Message>If verified, reply to the proposer <br />
+                            (PO-Reply, i, σVi)
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} display={level > 2 ? "block" : "none"}>
+                        <Message>If verified, reply to the proposer <br />
+                            (PO-Reply, i, σVi)
+                        </Message>
+                    </Grid>
+                    {/* Message level 4 */}
+                    <Grid item xs={3} display={level > 3 ? "block" : "none"}>
+                        <Message>Collect at least 2f PO-Reply <br />
+                            Convert σVi to (t, n)-threshold signatures σoi</Message>
+                    </Grid>
+                    <Grid item xs={3} display={level > 3 ? "block" : "none"}>
+                    </Grid>
+                    <Grid item xs={3} display={level > 3 ? "block" : "none"}>
+                    </Grid>
+                    <Grid item xs={3} display={level > 3 ? "block" : "none"}>
+                    </Grid>
+                    {/* Message level 5 */}
+                    <Grid item xs={3} display={level > 4 ? "block" : "none"}>
+                        <Message>Send Order Messages to all Validators <br />
+                            (Order, i, Qoi, σoi)</Message>
+                    </Grid>
+                    <Grid item xs={3} display={level > 4 ? "block" : "none"}>
+                    </Grid>
+                    <Grid item xs={3} display={level > 4 ? "block" : "none"}>
+                    </Grid>
+                    <Grid item xs={3} display={level > 4 ? "block" : "none"}>
+                    </Grid>
+                    {/* Message level 6 */}
+                    <Grid item xs={3} display={level > 5 ? "block" : "none"}>
+                        <Message>Append an order log entry <br />
+                            [Oi, Bi, Qoi, Vo, σoi]
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} display={level > 5 ? "block" : "none"}>
+                        <Message>Receive Order Message <br />
+                            Start to verify
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} display={level > 5 ? "block" : "none"}>
+                        <Message>Receive Order Message <br />
+                            Start to verify
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} display={level > 5 ? "block" : "none"}>
+                        <Message>Receive Order Message <br />
+                            Start to verify
+                        </Message>
+                    </Grid>
+                    {/* Message level 7 */}
+                    <Grid item xs={3} display={level > 6 ? "block" : "none"}>
+                    </Grid>
+                    <Grid item xs={3} display={level > 6 ? "block" : "none"}>
+                        <Message>If verified, append an order log entry <br />
+                            [Oi, Bi, Qoi, Vo, σoi]
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} display={level > 6 ? "block" : "none"}>
+                        <Message>If verified, append an order log entry <br />
+                            [Oi, Bi, Qoi, Vo, σoi]
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} display={level > 6 ? "block" : "none"}>
+                        <Message>If verified, append an order log entry <br />
+                            [Oi, Bi, Qoi, Vo, σoi]
+                        </Message>
+                    </Grid>
+                </Grid>
+                <br />
             </Container>
         </ThemeProvider>
         

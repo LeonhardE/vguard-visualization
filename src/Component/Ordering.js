f6aaaaadb0430215048f9e139e08495c5533aa76
LeonhardE <31626796+LeonhardE@users.noreply.github.com>
2023-04-07 21:35:34 -0400

    setup backdrop

---
+++ src/Component/Ordering.js
@@ -0,0 +1,261 @@
+import React from 'react';
+import { useState, useEffect, useRef } from 'react'
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+// import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { ThemeProvider } from '@mui/material/styles';
+import { Item, lightTheme, Message } from './Util';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import EmailIcon from '@mui/icons-material/Email';
+
+
+export default function Ordering() {
+    const [level, setlevel] = useState(0);
+
+    // auto scroll to bottom
+    const messagesEndRef = useRef(null)
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+    }
+    
+    useEffect(() => {
+        scrollToBottom()
+    }, [level]);
+
+    return (
+        <ThemeProvider theme={lightTheme}>
+            <CssBaseline />
+            <Box
+                sx={{
+                    bgcolor: 'background.paper',
+                    pt: 8,
+                    pb: 6,
+                }}
+                >
+                <Container maxWidth="sm">
+                    <Stack
+                    sx={{ pt: 4 }}
+                    direction="row"
+                    spacing={2}
+                    justifyContent="center"
+                    >
+                        <Button variant="contained" onClick={() => {
+                            setlevel(level + 1)
+                        }} >Next Step</Button>
+                        <Button variant="outlined" onClick={() => {
+                            setlevel(0)
+                        }} >Restart</Button>
+                    </Stack>
+                </Container>
+            </Box>
+            <Container maxWidth="100vw">
+                <Grid 
+                    container 
+                    justifyContent="center" 
+                    alignItems="center" 
+                    className="msgDisplay"
+                    spacing={2}>
+                    <Grid item xs={3}>
+                        <img className="vehicle" alt="vehicle" src="car.png" />
+                        <EmailIcon className="msg1" opacity={level === 1 || level === 5 ? 1 : 0} />
+                        <EmailIcon className="msg2" opacity={level === 1 || level === 5 ? 1 : 0} />
+                        <EmailIcon className="msg3" opacity={level === 1 || level === 5 ? 1 : 0} />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <img className="vehicle" alt="vehicle" src="car.png" />
+                        <EmailIcon className="msg4" 
+                            opacity={level === 3 ? 1 : 0} 
+                        />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <img className="vehicle" alt="vehicle" src="car.png" />
+                        <EmailIcon className="msg5" 
+                            opacity={level === 3 ? 1 : 0} 
+                        />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <img className="vehicle" alt="vehicle" src="car.png" />
+                        <EmailIcon className="msg6" 
+                            opacity={level === 3 ? 1 : 0} 
+                        />
+                    </Grid>
+                    {/* vehicle identities */}
+                    <Grid item xs={3}>
+                        <Item>Proposer</Item>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Item>Validator</Item>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Item>Validator</Item>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Item>Validator</Item>
+                    </Grid>
+                    {/* Message level 1 */}
+                    <Grid item xs={3} 
+                        // display = {level > 1 ? "none" : "block"} 
+                        className={level >= 1 ? "showmsg" : "hiddenmsg"}>
+                        <Message>Send PreOrder Messages to all Validators <br />
+                            (Pre-Order, Bi, hBi, Vo, hVo, i, σVp)</Message>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 1 ? "none" : "block"} 
+                        className={level >= 1 ? "showmsg" : "hiddenmsg"}>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 1 ? "none" : "block"} 
+                        className={level >= 1 ? "showmsg" : "hiddenmsg"}>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 1 ? "none" : "block"} 
+                        className={level >= 1 ? "showmsg" : "hiddenmsg"}>
+                    </Grid>
+                    {/* Message level 2 */}
+                    <Grid item xs={3} 
+                        // display = {level > 2 ? "none" : "block"} 
+                        className={level >= 2 ? "showmsg" : "hiddenmsg"}>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 2 ? "none" : "block"} 
+                        className={level >= 2 ? "showmsg" : "hiddenmsg"}>
+                        <Message>Receive PreOrder Message <br />
+                            Start to verify
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 2 ? "none" : "block"} 
+                        className={level >= 2 ? "showmsg" : "hiddenmsg"}>
+                        <Message>Receive PreOrder Message <br />
+                            Start to verify
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 2 ? "none" : "block"} 
+                        className={level >= 2 ? "showmsg" : "hiddenmsg"}>
+                        <Message>Receive PreOrder Message <br />
+                            Start to verify
+                        </Message>
+                    </Grid>
+                    {/* Message level 3 */}
+                    <Grid item xs={3} 
+                        // display = {level > 3 ? "none" : "block"} 
+                        className={level >= 3 ? "showmsg" : "hiddenmsg"}>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 3 ? "none" : "block"} 
+                        className={level >= 3 ? "showmsg" : "hiddenmsg"}>
+                        <Message>If verified, reply to the proposer <br />
+                            (PO-Reply, i, σVi)
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 3 ? "none" : "block"} 
+                        className={level >= 3 ? "showmsg" : "hiddenmsg"}>
+                        <Message>If verified, reply to the proposer <br />
+                            (PO-Reply, i, σVi)
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 3 ? "none" : "block"} 
+                        className={level >= 3 ? "showmsg" : "hiddenmsg"}>
+                        <Message>If verified, reply to the proposer <br />
+                            (PO-Reply, i, σVi)
+                        </Message>
+                    </Grid>
+                    {/* Message level 4 */}
+                    <Grid item xs={3} 
+                        // display = {level > 4 ? "none" : "block"} 
+                        className={level >= 4 ? "showmsg" : "hiddenmsg"}>
+                        <Message>Collect at least 2f PO-Reply <br />
+                            Convert σVi to (t, n)-threshold signatures σoi</Message>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 4 ? "none" : "block"}
+                        className={level >= 4 ? "showmsg" : "hiddenmsg"}>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 4 ? "none" : "block"}
+                        className={level >= 4 ? "showmsg" : "hiddenmsg"}>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 4 ? "none" : "block"}
+                        className={level >= 4 ? "showmsg" : "hiddenmsg"}>
+                    </Grid>
+                    {/* Message level 5 */}
+                    <Grid item xs={3} 
+                        // display = {level > 5 ? "none" : "block"}
+                        className={level >= 5 ? "showmsg" : "hiddenmsg"}>
+                        <Message>Send Order Messages to all Validators <br />
+                            (Order, i, Qoi, σoi)</Message>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 5 ? "none" : "block"}
+                        className={level >= 5 ? "showmsg" : "hiddenmsg"}>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 5 ? "none" : "block"}
+                        className={level >= 5 ? "showmsg" : "hiddenmsg"}>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 5 ? "none" : "block"}
+                        className={level >= 5 ? "showmsg" : "hiddenmsg"}>
+                    </Grid>
+                    {/* Message level 6 */}
+                    <Grid item xs={3} 
+                        // display = {level > 6 ? "none" : "block"}
+                        className={level >= 6 ? "showmsg" : "hiddenmsg"}>
+                        <Message>Append an order log entry <br />
+                            [Oi, Bi, Qoi, Vo, σoi]
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 6 ? "none" : "block"}
+                        className={level >= 6 ? "showmsg" : "hiddenmsg"}>
+                        <Message>Receive Order Message <br />
+                            Start to verify
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 6 ? "none" : "block"}
+                        className={level >= 6 ? "showmsg" : "hiddenmsg"}>
+                        <Message>Receive Order Message <br />
+                            Start to verify
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} 
+                        // display = {level > 6 ? "none" : "block"}
+                        className={level >= 6 ? "showmsg" : "hiddenmsg"}>
+                        <Message>Receive Order Message <br />
+                            Start to verify
+                        </Message>
+                    </Grid>
+                    {/* Message level 7 */}
+                    <Grid item xs={3} className={level >= 7 ? "showmsg" : "hiddenmsg"}>
+                    </Grid>
+                    <Grid item xs={3} className={level >= 7 ? "showmsg" : "hiddenmsg"}>
+                        <Message>If verified, append an order log entry <br />
+                            [Oi, Bi, Qoi, Vo, σoi]
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} className={level >= 7 ? "showmsg" : "hiddenmsg"}>
+                        <Message>If verified, append an order log entry <br />
+                            [Oi, Bi, Qoi, Vo, σoi]
+                        </Message>
+                    </Grid>
+                    <Grid item xs={3} className={level >= 7 ? "showmsg" : "hiddenmsg"}>
+                        <Message>If verified, append an order log entry <br />
+                            [Oi, Bi, Qoi, Vo, σoi]
+                        </Message>
+                    </Grid>
+                </Grid>
+                <div ref={messagesEndRef}/>
+                <br />
+            </Container>
+        </ThemeProvider>
+        
+      );
+}
--- src/Component/VGuard.js
+++ src/Component/VGuard.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect} from 'react'
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { ThemeProvider } from '@mui/material/styles';
-import { Item, lightTheme, Message } from './Util';
+import { lightTheme} from './Util';
 import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import EmailIcon from '@mui/icons-material/Email';
+// import Grid from '@mui/material/Grid';
+// import EmailIcon from '@mui/icons-material/Email';
+import Backdrop from '@mui/material/Backdrop';
+import Ordering from './Ordering';
+import Consensus from './Consensus'
 
 export default function VGuard() {
-    const [level, setlevel] = useState(0);
-
-    const messagesEndRef = useRef(null)
 
     async function testGET() {
         
@@ -44,16 +44,24 @@ export default function VGuard() {
         console.log(data);
     }
 
-    testGET();
-    testPOST();
+    useEffect(() => {
+        testGET();
+        testPOST();
+    }, []);
 
-    const scrollToBottom = () => {
-        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
-      }
-    
-      useEffect(() => {
-        scrollToBottom()
-      }, [level]);
+    const [open, setOpen] = useState([false, false]);
+    const handleClose = () => {
+        setOpen([false, false]);
+    };
+    const handleToggle = (key) => {
+        if (key === 0) {
+            setOpen([true, false]);
+        }
+        else {
+            setOpen([false, true]);
+        }
+    };
+
 
     return (
         <ThemeProvider theme={lightTheme}>
@@ -85,219 +93,26 @@ export default function VGuard() {
                     spacing={2}
                     justifyContent="center"
                     >
-                        <Button variant="contained" onClick={() => {
-                            setlevel(level + 1)
-                        }} >Next Step</Button>
-                        <Button variant="outlined" onClick={() => {
-                            setlevel(0)
-                        }} >Restart</Button>
+                        <Button variant="contained" onClick={() => handleToggle(0)} >Ordering</Button>
+                        <Button variant="outlined" onClick={() => handleToggle(1)} >Consensus</Button>
                     </Stack>
+                    <Backdrop
+                      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                      open={open[0]}
+                    >
+                        <Button onClick={ () => handleClose() }>Back</Button>
+                        <Ordering />
+                    </Backdrop>
+                    <Backdrop
+                      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                      open={open[1]}
+                    >
+                        <Button onClick={ () => handleClose() }>Back</Button>
+                        <Consensus />
+                    </Backdrop>
                 </Container>
             </Box>
-            <Container maxWidth="100vw">
-                <Grid 
-                    container 
-                    justifyContent="center" 
-                    alignItems="center" 
-                    className="msgDisplay"
-                    spacing={2}>
-                    <Grid item xs={3}>
-                        <img className="vehicle" alt="vehicle" src="car.png" />
-                        <EmailIcon className="msg1" opacity={level === 1 || level === 5 ? 1 : 0} />
-                        <EmailIcon className="msg2" opacity={level === 1 || level === 5 ? 1 : 0} />
-                        <EmailIcon className="msg3" opacity={level === 1 || level === 5 ? 1 : 0} />
-                    </Grid>
-                    <Grid item xs={3}>
-                        <img className="vehicle" alt="vehicle" src="car.png" />
-                        <EmailIcon className="msg4" 
-                            opacity={level === 3 ? 1 : 0} 
-                        />
-                    </Grid>
-                    <Grid item xs={3}>
-                        <img className="vehicle" alt="vehicle" src="car.png" />
-                        <EmailIcon className="msg5" 
-                            opacity={level === 3 ? 1 : 0} 
-                        />
-                    </Grid>
-                    <Grid item xs={3}>
-                        <img className="vehicle" alt="vehicle" src="car.png" />
-                        <EmailIcon className="msg6" 
-                            opacity={level === 3 ? 1 : 0} 
-                        />
-                    </Grid>
-                    {/* vehicle identities */}
-                    <Grid item xs={3}>
-                        <Item>Proposer</Item>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Item>Validator</Item>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Item>Validator</Item>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Item>Validator</Item>
-                    </Grid>
-                    {/* Message level 1 */}
-                    <Grid item xs={3} 
-                        // display = {level > 1 ? "none" : "block"} 
-                        className={level >= 1 ? "showmsg" : "hiddenmsg"}>
-                        <Message>Send PreOrder Messages to all Validators <br />
-                            (Pre-Order, Bi, hBi, Vo, hVo, i, σVp)</Message>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 1 ? "none" : "block"} 
-                        className={level >= 1 ? "showmsg" : "hiddenmsg"}>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 1 ? "none" : "block"} 
-                        className={level >= 1 ? "showmsg" : "hiddenmsg"}>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 1 ? "none" : "block"} 
-                        className={level >= 1 ? "showmsg" : "hiddenmsg"}>
-                    </Grid>
-                    {/* Message level 2 */}
-                    <Grid item xs={3} 
-                        // display = {level > 2 ? "none" : "block"} 
-                        className={level >= 2 ? "showmsg" : "hiddenmsg"}>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 2 ? "none" : "block"} 
-                        className={level >= 2 ? "showmsg" : "hiddenmsg"}>
-                        <Message>Receive PreOrder Message <br />
-                            Start to verify
-                        </Message>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 2 ? "none" : "block"} 
-                        className={level >= 2 ? "showmsg" : "hiddenmsg"}>
-                        <Message>Receive PreOrder Message <br />
-                            Start to verify
-                        </Message>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 2 ? "none" : "block"} 
-                        className={level >= 2 ? "showmsg" : "hiddenmsg"}>
-                        <Message>Receive PreOrder Message <br />
-                            Start to verify
-                        </Message>
-                    </Grid>
-                    {/* Message level 3 */}
-                    <Grid item xs={3} 
-                        // display = {level > 3 ? "none" : "block"} 
-                        className={level >= 3 ? "showmsg" : "hiddenmsg"}>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 3 ? "none" : "block"} 
-                        className={level >= 3 ? "showmsg" : "hiddenmsg"}>
-                        <Message>If verified, reply to the proposer <br />
-                            (PO-Reply, i, σVi)
-                        </Message>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 3 ? "none" : "block"} 
-                        className={level >= 3 ? "showmsg" : "hiddenmsg"}>
-                        <Message>If verified, reply to the proposer <br />
-                            (PO-Reply, i, σVi)
-                        </Message>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 3 ? "none" : "block"} 
-                        className={level >= 3 ? "showmsg" : "hiddenmsg"}>
-                        <Message>If verified, reply to the proposer <br />
-                            (PO-Reply, i, σVi)
-                        </Message>
-                    </Grid>
-                    {/* Message level 4 */}
-                    <Grid item xs={3} 
-                        // display = {level > 4 ? "none" : "block"} 
-                        className={level >= 4 ? "showmsg" : "hiddenmsg"}>
-                        <Message>Collect at least 2f PO-Reply <br />
-                            Convert σVi to (t, n)-threshold signatures σoi</Message>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 4 ? "none" : "block"}
-                        className={level >= 4 ? "showmsg" : "hiddenmsg"}>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 4 ? "none" : "block"}
-                        className={level >= 4 ? "showmsg" : "hiddenmsg"}>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 4 ? "none" : "block"}
-                        className={level >= 4 ? "showmsg" : "hiddenmsg"}>
-                    </Grid>
-                    {/* Message level 5 */}
-                    <Grid item xs={3} 
-                        // display = {level > 5 ? "none" : "block"}
-                        className={level >= 5 ? "showmsg" : "hiddenmsg"}>
-                        <Message>Send Order Messages to all Validators <br />
-                            (Order, i, Qoi, σoi)</Message>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 5 ? "none" : "block"}
-                        className={level >= 5 ? "showmsg" : "hiddenmsg"}>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 5 ? "none" : "block"}
-                        className={level >= 5 ? "showmsg" : "hiddenmsg"}>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 5 ? "none" : "block"}
-                        className={level >= 5 ? "showmsg" : "hiddenmsg"}>
-                    </Grid>
-                    {/* Message level 6 */}
-                    <Grid item xs={3} 
-                        // display = {level > 6 ? "none" : "block"}
-                        className={level >= 6 ? "showmsg" : "hiddenmsg"}>
-                        <Message>Append an order log entry <br />
-                            [Oi, Bi, Qoi, Vo, σoi]
-                        </Message>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 6 ? "none" : "block"}
-                        className={level >= 6 ? "showmsg" : "hiddenmsg"}>
-                        <Message>Receive Order Message <br />
-                            Start to verify
-                        </Message>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 6 ? "none" : "block"}
-                        className={level >= 6 ? "showmsg" : "hiddenmsg"}>
-                        <Message>Receive Order Message <br />
-                            Start to verify
-                        </Message>
-                    </Grid>
-                    <Grid item xs={3} 
-                        // display = {level > 6 ? "none" : "block"}
-                        className={level >= 6 ? "showmsg" : "hiddenmsg"}>
-                        <Message>Receive Order Message <br />
-                            Start to verify
-                        </Message>
-                    </Grid>
-                    {/* Message level 7 */}
-                    <Grid item xs={3} className={level >= 7 ? "showmsg" : "hiddenmsg"}>
-                    </Grid>
-                    <Grid item xs={3} className={level >= 7 ? "showmsg" : "hiddenmsg"}>
-                        <Message>If verified, append an order log entry <br />
-                            [Oi, Bi, Qoi, Vo, σoi]
-                        </Message>
-                    </Grid>
-                    <Grid item xs={3} className={level >= 7 ? "showmsg" : "hiddenmsg"}>
-                        <Message>If verified, append an order log entry <br />
-                            [Oi, Bi, Qoi, Vo, σoi]
-                        </Message>
-                    </Grid>
-                    <Grid item xs={3} className={level >= 7 ? "showmsg" : "hiddenmsg"}>
-                        <Message>If verified, append an order log entry <br />
-                            [Oi, Bi, Qoi, Vo, σoi]
-                        </Message>
-                    </Grid>
-                </Grid>
-                <div ref={messagesEndRef}/>
-                <br />
-            </Container>
+            
         </ThemeProvider>
         
       );
